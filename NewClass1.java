ed423da8aedf0b36114683941d2d6ec6fb95f207
skhud <40534778+skhud@users.noreply.github.com>
2022-07-03 19:52:47 +0300

    Update NewClass1.java

--- NewClass1.java
+++ NewClass1.java
@@ -1,10 +1,6 @@
 public class NewClass1 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("test branch");
-=======
-        System.out.println("main branch");
->>>>>>> main
-
-    }
+    System.out.println("test branch");
+    System.out.println("main branch");
+   }
 }
